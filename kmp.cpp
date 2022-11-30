ceb2ee26ad9fe90e55d5d815a5cb710deddfff58
Egon <111221887+lijiahao2022@users.noreply.github.com>
2022-11-30 19:05:36 +0800

    Update kmp.cpp

--- kmp.cpp
+++ kmp.cpp
@@ -24,7 +24,39 @@ int main()
 	getnext(next, s);
 	for (int i = 1; i <=strlen(s); i++)
 	{
+		next[i]=next[i-1];//由最长前后缀数组向右移一位得到next数组
+	}
+	next[0]=-1;
+		for (int i = 1; i <=strlen(s); i++)
+	{
 		cout << next[i]<<" ";
 	}
 
 }
+//利用next数组可实现kmp算法
+// int KmpSearch(char* s, char* p)
+// {
+// 	int i = 0;
+// 	int j = 0;
+// 	int sLen = strlen(s);
+// 	int pLen = strlen(p);
+// 	while (i < sLen && j < pLen)
+// 	{
+// 		//①如果j = -1，或者当前字符匹配成功（即S[i] == P[j]），都令i++，j++    
+// 		if (j == -1 || s[i] == p[j])
+// 		{
+// 			i++;
+// 			j++;
+// 		}
+// 		else
+// 		{
+// 			//②如果j != -1，且当前字符匹配失败（即S[i] != P[j]），则令 i 不变，j = next[j]    
+// 			//next[j]即为j所对应的next值      
+// 			j = next[j];
+// 		}
+// 	}
+// 	if (j == pLen)
+// 		return i - j;
+// 	else
+// 		return -1;
+// }
